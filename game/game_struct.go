29001b7af9ae6b04c75eca40ad0c0196034beae2
Illia Karenhin <illia.karenhin@raiffeisen.ua>
2023-09-13 10:37:35 +0300

    HW3: in progress

---
+++ game/game_struct.go
@@ -0,0 +1,7 @@
+package game
+
+type game struct {
+}
+
+type question struct {
+}
--- zoo/zoo.go
+++ zoo/zoo.go
@@ -80,6 +80,7 @@ func getAnimalsInfo(animal string) []Animal {
 	if err != nil {
 		fmt.Println(err)
 	}
+	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
